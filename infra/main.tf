b4423cc073089fac7a0f440c54de587fffd4d9a5
Asdrubal82212 <93086798+Asdrubal82212@users.noreply.github.com>
2026-03-22 01:56:41 +0000

    Configurando bucket como sitio web estatico

--- infra/main.tf
+++ infra/main.tf
@@ -10,3 +10,38 @@ resource "aws_s3_bucket" "site_bucket" {
     ManagedBy   = "Terraform"
   }
 }
+
+resource "aws_s3_bucket_website_configuration" "site_config" {
+  bucket = aws_s3_bucket.site_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "site_public" {
+  bucket = aws_s3_bucket.site_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "site_policy" {
+  bucket = aws_s3_bucket.site_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.site_bucket.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.site_public]
+}
